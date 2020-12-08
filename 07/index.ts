6871da9bd28ca1effa29647b89a4f745a18cbe26
Michael Siadak <mike.siadak@gmail.com>
2020-12-07 19:21:49 -0700

    Day 7 part 2

--- 07/index.ts
+++ 07/index.ts
@@ -58,4 +58,31 @@ for (const description of Object.keys(bagMap)) {
   }
 }
 
-console.log(found);
+console.log(
+  `Number of bag types that eventually may contain a shiny gold bag: ${found}`
+);
+
+function numBagsInShinyGoldBag(): number {
+  const memo: Record<string, number> = {};
+
+  function numBags(bagDescription: string): number {
+    if (memo.hasOwnProperty(bagDescription)) {
+      return memo[bagDescription];
+    }
+
+    const children = bagMap[bagDescription];
+
+    // Start with the containing bag itself
+    let total = 1;
+    for (const child of Object.keys(children)) {
+      total += numBags(child) * children[child];
+    }
+
+    memo[bagDescription] = total;
+    return total;
+  }
+
+  return numBags('shiny gold') - 1;
+}
+
+console.log(numBagsInShinyGoldBag());
