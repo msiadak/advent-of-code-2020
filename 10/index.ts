b94bb171ddedb9d62f74f2b4a0ee099f152fccdb
Michael Siadak <mike.siadak@gmail.com>
2020-12-10 00:41:28 -0700

    Day 10 part 2

--- 10/index.ts
+++ 10/index.ts
@@ -24,3 +24,22 @@ for (let i = 1; i < adapters.length; i++) {
 }
 
 console.log('Part 1:', oneJoltDiffs * threeJoltDiffs);
+
+const finalJoltage = adapters[adapters.length - 1];
+const dp = new Array(finalJoltage + 1).fill(0);
+dp[0] = 1;
+const adapterSet = new Set(adapters);
+
+for (const adapter of adapters) {
+  if (adapterSet.has(adapter + 1)) {
+    dp[adapter + 1] += dp[adapter];
+  }
+  if (adapterSet.has(adapter + 2)) {
+    dp[adapter + 2] += dp[adapter];
+  }
+  if (adapterSet.has(adapter + 3)) {
+    dp[adapter + 3] += dp[adapter];
+  }
+}
+
+console.log(dp[finalJoltage]);
